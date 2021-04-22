8bc771b708a110f8f7b2a1a7f69e258e2c707571
信长华 <skywater@gmail.com>
2021-04-22 20:39:16 +0800

    [tuning][子模块Test][end] 每个子模块添加Test功能

---
+++ controller/src/test/java/com/se/back/ControllerApplication.java
@@ -0,0 +1,27 @@
+package com.se.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: 信长华
+ * @date: 2021/4/22 20:14
+ * @className: ControllerApplication
+ * @description: TODO
+ * @version: 1.0
+ */
+
+
+/**
+ * 这个启动类是用来做测试的
+ * 必须放在测试目录下，不然会和其他模块冲突
+ * 一山不容二 @SpringBootApplication
+ * https://stackoverflow.com/questions/37615914
+ * 参考 https://github.com/fish-springboot/SpringBootModules
+ */
+@SpringBootApplication
+public class ControllerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ControllerApplication.class, args);
+    }
+}
